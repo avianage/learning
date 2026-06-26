67d11fe54b4d74cac5af8fba31c7094b49cf29bf
avianage <aakash.joshi.cs@gmail.com>
2026-06-26 16:05:46 +0530

    learn: Code till Pattern 17 done

---
+++ dsa/1.basics/patterns/pattern15.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n = 5;
+    for (int i = n; i > 0; i--){
+        for (char ch = 'A'; ch < 'A' + i; ch++){
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+
+}
