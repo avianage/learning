92ffba3459ea5227ac33f8d780f11d1c5103fc34
avianage <aakash.joshi.cs@gmail.com>
2026-07-06 18:59:06 +0530

    chore: added 100xdev resources

---
+++ 100xDev Bootcamp/Web3/13. Rust for Solana/src/main.rs
@@ -0,0 +1,23 @@
+
+// Basic Program
+// fn main() {
+//     let x: u32 = 10;
+//     println!("{}", x)   
+// }
+
+// fn main() {
+//     let mut greeting: String::from("hello world")
+// }
+
+
+// Arrays
+fn main() {
+    let arr: [i32, 5] = [1 , 2 , 3 , 4 , 5];
+    println!("{}", arr.len());
+}
+
+// Vectors
+fn main(){
+    let mut vec: Vec<i32> = vec![1 , 2 , 3 , 4 , 5];
+    println!("{}"
+}
