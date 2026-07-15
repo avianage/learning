cf8d43d57c3d2cfac8720b1be1fa592836c6f471
avianage <aakash.joshi.cs@gmail.com>
2026-07-15 18:07:34 +0530

    revise: Started Java Revision under Cloud Full Stack Development Training

---
+++ Cloud Full Stack Development Training/Core Java/Code/src/main/java/com/ibm/revision/day1/methods/MethodDemo.java
@@ -0,0 +1,11 @@
+package com.ibm.revision.day1.methods;
+
+public class MethodDemo {
+    void printNums() {
+        for (int i = 1; i <= 5; i++){
+            System.out.println(i);
+        }
+    }
+
+    static void
+}
