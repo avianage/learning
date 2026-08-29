d40bb71413e5301b21df9c861ceb913983fbb278
Avianage <aakash.joshi.cs@gmail.com>
2026-08-29 19:27:16 +0530

    learn: More Validations

--- Playwright Automation Testing using JS/tests/Validations.spec.js
+++ Playwright Automation Testing using JS/tests/Validations.spec.js
@@ -1,22 +1,29 @@
 import {test, expect} from "@playwright/test"
 
 test.only('More Validations', async ({page}) => {
-    await page.goto("https://eventhub.rahulshettyacademy.com");
-    await page.goto("https://google.com")
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice");
+    // await page.goto("https://google.com")
 
     // Use to go back to the previous page
-    await page.goBack();
+    // await page.goBack();
 
     // Use to navigate to forward page
-    await page.goForward();
+    // await page.goForward();
 
     // Handling JS Dialogs
     page.on('dialog', dialog => dialog.accept()); // For accepting
     // page.on('dialog', dialog => dialog.dismiss()); // For rejecting
     
-
-    // use .hover() to hover 
+    await page.locator("#confirmbtn").click();
     
+    // use .hover() to hover 
+    await page.locator("#mousehover").hover();
 
+    const framesPage = page.frameLocator("#courses-iframe")
+
+    await framesPage.locator("li a[href*='lifetime-access']:visible").click();
+    const textCheck = await  framesPage.locator(".text h2").textContent()
+
+    console.log(textCheck.split(" ")[1]);
 
 })
